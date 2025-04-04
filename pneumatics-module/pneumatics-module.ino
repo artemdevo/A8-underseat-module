de4ca4ef588d4d7c1092c857caec98e3a984c2c4
artemdevo <70590010+artemdevo@users.noreply.github.com>
2025-04-04 17:12:16 -0400

    Update pneumatics-module.ino

--- pneumatics-module/pneumatics-module.ino
+++ pneumatics-module/pneumatics-module.ino
@@ -1,12 +1,18 @@
-
+#include <SPI.h>
+#include <mcp_can.h>
 
 
 #define COMP 10
+#define VENT 23
 
 
 void setup() {
   // put your setup code here, to run once:
 
+
+
+  CAN0.begin(MCP_ANY, CAN_500KBPS, MCP_8MHZ);
+  CAN0.setMode(MCP_NORMAL);
 }
 
 void loop() {
