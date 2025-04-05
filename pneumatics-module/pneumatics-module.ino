988176b6cce19aebc56a81c679a5f6daf7845edc
artemdevo <70590010+artemdevo@users.noreply.github.com>
2025-04-05 03:08:38 -0400

    Update pneumatics-module.ino

--- pneumatics-module/pneumatics-module.ino
+++ pneumatics-module/pneumatics-module.ino
@@ -50,3 +50,82 @@ void loop() {
   
   
 }
+
+
+/* 
+ 
+
+ byte massagestate; //no need to assign this a value, it should start at state 0
+ unsigned long statestart; //set this equal to millis() when the massage starts
+ unsigned int delaytime; //give this an initial value
+
+ byte massagestatus; //this will be another global value stating if massage is on, and if so, which program to run. massagestatus will be set by reading CAN messages
+
+  if(massagestatus){
+    if(millis()-statestart > delaytime){ //
+      if(massagestate == MAX_MASSAGE_STATE){
+        massagestate=0;   
+      }
+      else{
+        massagestate++;
+      }
+    
+      statestart = millis();
+
+      if(massagestate%2 == 0){ //setting the delay time to the next state here 
+        delaytime = 200;
+      }
+      else{
+        delaytime = 400;
+      }
+    }
+
+    switch(massagestate){
+    
+      case 0:{
+        //assign pins to be high here, and assign pins from the previous state to be low (if applicable);
+      }
+      break;
+      case 1:{
+      }
+      break;
+      case 2:{
+        myDFPlayer.play(3);
+      }
+  
+    }
+  }
+  else{
+    switch(massagestate){
+    
+      case 0:{
+        //this is a second switch case that for a given state above, this case sets the pins in that particular massage state low. This 
+        //is to set massage pins low when massagestatus goes to 0, and its faster than setting every single massage pin to 0 in a for loop
+        //on second thought, maybe i should just put a for loop here and set everything to LOW?
+        
+      }
+      break;
+      case 1:{
+      }
+      break;
+      case 2:{
+        myDFPlayer.play(3);
+      }
+
+
+
+
+
+
+  }
+
+
+
+
+
+
+
+
+
+
+
