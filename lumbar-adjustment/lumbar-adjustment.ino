40e444b2a974bc070fa8a0568e6cc312eddfaa26
artemdevo <70590010+artemdevo@users.noreply.github.com>
2025-04-06 15:11:52 -0400

    Update lumbar-adjustment.ino

--- lumbar-adjustment/lumbar-adjustment.ino
+++ lumbar-adjustment/lumbar-adjustment.ino
@@ -1,9 +1,42 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <EEPROM.h>
+
+// start reading from the first byte (address 0) of the EEPROM
+int address = 0;
+byte value;
+
+void setup() {
+  // initialize serial and wait for port to open:
+  Serial.begin(9600);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // read a byte from the current address of the EEPROM
+  value = EEPROM.read(address);
 
+  Serial.print(address);
+  Serial.print("\t");
+  Serial.print(value, DEC);
+  Serial.println();
+
+  /***
+    Advance to the next address, when at the end restart at the beginning.
+
+    Larger AVR processors have larger EEPROM sizes, E.g:
+    - Arduino Duemilanove: 512 B EEPROM storage.
+    - Arduino Uno:         1 kB EEPROM storage.
+    - Arduino Mega:        4 kB EEPROM storage.
+
+    Rather than hard-coding the length, you should use the pre-provided length function.
+    This will make your code portable to all AVR processors.
+  ***/
+  address = address + 1;
+  if (address == EEPROM.length()) {
+    address = 0;
+  }
+
+
+
+  delay(500);
 }
